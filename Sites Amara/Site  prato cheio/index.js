6f2abbe7bbb4047c0e8c05a4426fd2df28e76a1a
Amara-Siqueira <siqueiraamara6@gmail.com>
2024-10-17 11:00:30 -0300

    att
    att pag principal

---
+++ Sites Amara/Site  prato cheio/index.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+    var menu = document.getElementById('menu');
+    if (menu.style.display === 'none') {
+        menu.style.display = 'block'; // Mostra o menu
+    } else {
+        menu.style.display = 'none';  // Oculta o menu
+    }
+}
+function menu() {
+    let Menu = document.getElementById('menu1');
+    if (menu1.style.display == 'none'){
+        menu1.style.display = 'block';
+    }else {
+        menu1.style.display = 'none';
+    }
+
+}
